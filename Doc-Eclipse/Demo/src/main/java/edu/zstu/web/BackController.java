2076c17ae71115a27964505dc586aba30d70d0f1
Jocoboy <Jocoboy@outlook.com>
2021-06-05 15:44:03 +0800

    spring data jpa app [3] : last commit -  now you can modify password of your own.

--- Doc-Eclipse/Demo/src/main/java/edu/zstu/web/BackController.java
+++ Doc-Eclipse/Demo/src/main/java/edu/zstu/web/BackController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springside.modules.security.utils.Digests;
+import org.springside.modules.utils.Encodes;
 
 import edu.zstu.entity.FoodCollectEntity;
 import edu.zstu.entity.UserEntity;
@@ -42,5 +44,37 @@ public class BackController {
 			return false;
 		}
 		return false;
+	}
+	
+	
+	@RequestMapping(value="modifyPassword",method = RequestMethod.POST)
+	public @ResponseBody Boolean modifyPassword
+	(
+				/*@RequestParam(value="email") String username,*/
+				/*@RequestParam(value="password-old")*/ String oldPassword,
+				/*@RequestParam(value="password-new")*/ String newPassword
+				/*@RequestParam(value="password-new-confirm") String newPasswordConfirm*/
+	){
+		UserEntity user = this.userService.getCurrUser();
+//		if(username == user.getUsername()){
+			String encodeOldPassword1 = user.getPassword();
+			String salt = user.getSalt();
+			byte[] salts = Encodes.decodeHex(salt);
+			
+			byte[] hashOldPassword = Digests.sha1(oldPassword.getBytes(), salts , 1024); 
+			String encodeOldPassword2 = Encodes.encodeHex(hashOldPassword);
+			System.out.println("encodeOldPassword1: "+encodeOldPassword1 + "\nencodeOldPassword2: " + encodeOldPassword2);
+			if(encodeOldPassword1.equals(encodeOldPassword2)){
+				byte[] hashNewPassword= Digests.sha1(newPassword.getBytes(), salts, 1024); 
+				String encodeNewPassword = Encodes.encodeHex(hashNewPassword);
+				user.setPassword(encodeNewPassword);
+				user.setRemark("存在修改记录");
+				userService.save(user);
+				return true;
+			}
+//		}
+		
+		return false;
+		
 	}
 }
